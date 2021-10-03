d4e69f6f2a72fc2dee22f7f7b2e2ab00730fdae9
Kamil Grajko <kamilgrajko@interia.pl>
2021-10-03 21:32:02 +0200

    add first file

---
+++ app.py
@@ -0,0 +1,119 @@
+from classes import mg
+from flask import Flask,render_template,request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Products(db.Model):
+    name = db.Column(db.String(50),primary_key=True,)
+    amount = db.Column(db.Integer,nullable=False, unique=False)
+
+class Action(db.Model):
+    pk = db.Column(db.Integer, primary_key=True)
+    action = db.Column(db.String(15),nullable=False,unique=False)
+    kwota = db.Column(db.Integer, nullable=True, unique=False)
+    comment = db.Column(db.String(30), nullable=True, unique=False)
+    product = db.Column(db.String(30), nullable=True, unique=False)
+    price = db.Column(db.Integer, nullable=True, unique=False)
+    qty = db.Column(db.Integer, nullable=True, unique=False)
+    saldo = db.Column(db.Integer, nullable=False, unique=False)
+
+db.create_all()
+
+def update_manager():
+    actions=db.session.query(Action).all()
+    for action in actions:
+        if action.action =="saldo":
+            params=[action.kwota,action.comment]
+        else:
+            params=[action.product,action.price,action.qty]
+        mg.commands.append((action.action,params))
+
+update_manager()
+mg.execute()
+
+@app.route("/", methods=["get","post"])
+def index():
+    saldo = mg.saldo
+    products = mg.magazyn
+    product_list =[]
+    for product in products:
+        product_list.append((product,products[product]))
+    return render_template("index.html", saldo=saldo ,products=product_list)
+
+@app.route("/resp/", methods=["post","get"])
+def resp():
+    resp_dict=dict(request.form)
+    wplata= resp_dict["wplata"]
+    komentarz = resp_dict["komentarz"]
+    params=[wplata,komentarz]
+    mg.callbacks[resp_dict["action"]](params)
+    saldo = mg.saldo
+    products = mg.magazyn
+    product_list = []
+    for product in products:
+        product_list.append((product, products[product]))
+    command_resp = []
+    for i in resp_dict.values():
+        command_resp.append(i)
+    mg.commands.append((command_resp[0], command_resp[1:]))
+    return render_template("index.html", saldo=saldo, products=product_list)
+
+
+@app.route("/resp1/", methods=["post","get"])
+def resp1():
+    resp_dict=dict(request.form)
+    nazwa = resp_dict["nazwa_produktu"]
+    cena = resp_dict["cena"]
+    ilosc = resp_dict["ilosc"]
+    params=[nazwa,cena,ilosc]
+    mg.callbacks[resp_dict["action1"]](params)
+    saldo = mg.saldo
+    products = mg.magazyn
+    product_list = []
+    for product in products:
+        product_list.append((product, products[product]))
+    command_resp = []
+    for i in resp_dict.values():
+        command_resp.append(i)
+    mg.commands.append((command_resp[0], command_resp[1:]))
+    return render_template("index.html", saldo=saldo,products=product_list)
+
+@app.route("/resp2/", methods=["post","get"])
+def resp2():
+    resp_dict=dict(request.form)
+    nazwa = resp_dict["nazwa_produktu"]
+    cena = resp_dict["cena"]
+    ilosc = resp_dict["ilosc"]
+    params=[nazwa,cena,ilosc]
+    mg.callbacks[resp_dict["action2"]](params)
+    saldo = mg.saldo
+    products = mg.magazyn
+    product_list = []
+    for product in products:
+        product_list.append((product, products[product]))
+    command_resp=[]
+    for i in resp_dict.values():
+        command_resp.append(i)
+    mg.commands.append((command_resp[0],command_resp[1:]))
+    return render_template("index.html", saldo=saldo, products=product_list)
+
+@app.route("/resp3/",methods=["get","post"])
+def resp3():
+    resp_dict = dict(request.form)
+    od = int(resp_dict["od"])
+    do = int(resp_dict["do"])
+    historia = mg.commands
+    historia_list = []
+    for transakcja in historia:
+        historia_list.append(transakcja)
+    if od and do == " ":
+        historia_list=historia_list
+    else:
+        historia_list=historia_list[od:do]
+    return render_template("historia.html" ,historia=historia_list,od=od,do=do)
+
+
---
+++ classes.py
@@ -0,0 +1,97 @@
+import sys
+
+class Product:
+    def __init__(self,name):
+        self.name= name
+        self.qty = 0
+
+class Manager:
+
+    def __init__(self):
+        self.callbacks = {}
+        self.action_args = {}
+        self.saldo = 0
+        self.commands = []
+        self.magazyn = {}
+        self.count = []
+
+    def asign(self, action, count):
+        def decorate(callback):
+            self.callbacks[action] = callback
+            self.action_args[action] = count
+
+        return decorate
+
+    def create_commands(self, action_list):
+        while action_list:
+            action = action_list.pop(0)
+            self.count.append(action)
+            params = []
+            # print(action_list)
+            if action in self.action_args:
+                for i in range(self.action_args[action]):
+                    # print(i)
+                    params.append(action_list.pop(0))
+                self.commands.append((action, params))
+            for i in params:
+                self.count.append(i)
+
+    def execute(self):
+        for action, params in self.commands:
+            if action in self.callbacks:
+                self.callbacks[action](params)
+
+
+class File:
+
+    def __init__(self,file_pathin):
+        self.file_pathin = file_pathin
+        self.actions = []
+
+    def file_reader(self,file_pathin):
+        with open(self.file_pathin, "r") as file:
+            for line in file:
+                self.actions.append(line.strip())
+# import_file = "in.txt"
+mg = Manager()
+# fr= File(file_pathin=import_file)
+
+# fr.file_reader(fr.file_pathin)
+
+@mg.asign("saldo", 2)
+def saldo(params):
+    value = int(params[0])
+    comment = str(params[1])
+    if mg.saldo + value < 0:
+        print("bledna kwota")
+    else:
+        mg.saldo += value
+
+
+@mg.asign("kupno", 3)
+def kupno(params):
+    name = params[0]
+    price = params[1]
+    qty = int(params[2])
+    mg.saldo -= int(price) * int(qty)
+    if name in mg.magazyn.keys():
+        mg.magazyn[name] += int(qty)
+    else:
+        mg.magazyn[name] = int(qty)
+
+
+@mg.asign("sprzedaz", 3)
+def sprzedaz(params):
+    name = params[0]
+    price = params[1]
+    qty = params[2]
+    mg.saldo += int(price) * int(qty)
+    if name in mg.magazyn.keys():
+        mg.magazyn[name] -= int(qty)
+    else:
+        mg.magazyn[name] = int(qty) - (2 * int(qty))
+    # if mg.magazyn[name]<0:
+    #     print("Wartość ujemna")
+    #     exit()
+# mg.create_commands(fr.actions)
+# mg.execute()
